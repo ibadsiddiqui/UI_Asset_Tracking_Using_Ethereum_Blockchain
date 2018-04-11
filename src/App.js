2ee0640d921cc2e218b3074dfe9837f6a77bc2c5
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-04-11 21:06:59 +0500

    functions binded

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {};
+    this.submitAsset = this.submitAsset.bind(this);
+    this.getAsset = this.getAsset.bind(this);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
