d2650a140003484e6a715471f912b5170e7f20d6
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-04-14 20:45:57 +0500

    states and UI updated for asset details component

--- src/component/getAssetDetails.js
+++ src/component/getAssetDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../App.css';
+import './css/getAssetDetails.css';
 
 
 import Automobile_Contract from '../auto_mobile';
@@ -8,17 +8,27 @@ import web3 from '../web3';
 export default class AssetDetails extends Component {
     constructor(){
         super();
-        this.state = {};
+        this.state = {
+            searchDetails:'',
+            assetDetails:[]
+        };
         this.getSpecificAsset = this.getSpecificAsset.bind(this);
     }
 
-    async getSpecificAsset() {
-        let specificAsset = {};
-        // const {searchDetails} = this.state
-        await Automobile_Contract.methods.getSpecificCar( parseInt(this.state.searchDetails)).call().then(console.log)
+    async getSpecificAsset(e) {
+        e.preventDefault();
+        const {searchDetails} = this.state;
+        await Automobile_Contract.methods.getSpecificCar( parseInt(searchDetails)).call()
+            .then( (result) => {
+                this.setState({
+                    assetDetails: result
+                })
+            });
+            console.log(this.state.assetDetails)
     }
 
     render() {
+        const {assetDetails} = this.state;
         return (
             <div className="col">
                 <div className="card">
@@ -26,7 +36,37 @@ export default class AssetDetails extends Component {
                         <h5 className="card-title">Get Details Of Asset {this.state.manufacturer_name}</h5>
                         <input type="text" name="detais" className="form-control" onChange={(e) => this.setState({searchDetails: e.target.value})}/>                
                         <button onClick={this.getSpecificAsset} className="btn btn-primary">Submit</button>
-                        {/* <p>{this.state.list_of_asset[0]}</p> */}
+                        <h6>
+                            Lot Number:
+                            <span className="lotNumber">
+                                {assetDetails[0]}
+                            </span>
+                        </h6> 
+                        <h6>
+                            Color: 
+                            <span className="colorClass">
+                                {assetDetails[1]}
+                            </span>
+                        </h6>
+                        <h6>
+                            Engine Type: 
+                            <span className="engineType">
+                                {assetDetails[2]}
+                            </span>
+                        </h6> 
+                        <h6>
+                            Date of Manufacture: 
+                            <span className="dateOfManufacturer">
+                                {assetDetails[3]}
+                            </span>
+                        </h6> 
+                        <h6>
+                            Manufacturing Origin: 
+                            <span className="mOrigin">
+                                {assetDetails[4]}
+                            </span>
+                        </h6> 
+
                     </div>
                 </div>
             </div>
