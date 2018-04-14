bda036b5c06594a778b2174cf977433b03f1f5e0
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-04-14 20:46:41 +0500

    Styling added

--- src/component/getCurrentOwner.js
+++ src/component/getCurrentOwner.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../App.css';
+import './css/getCurrentOwner.css';
 
 import Automobile_Contract from '../auto_mobile';
 import web3 from '../web3';
@@ -29,18 +29,18 @@ export default class GetCurrentOwner extends Component {
             <div className="col">
                 <div className="card">
                     <div className="card-body">
-                        <h5 className="card-title">Get Details Of Asset: {this.state.VINNumber}</h5>
+                        <h5 className="card-title">Get Current Owner Of Asset: {this.state.VINNumber}</h5>
                         <input type="text" name="detais" className="form-control" onChange={(e) => this.setState({VINNumber: e.target.value})}/>                
                         <button onClick={this.getCurrentOwner} className="btn btn-primary">Submit</button>
                         <h6>
                             Current Owner: 
-                            <span>
+                            <span className="currentOwner">
                                 {this.state.OwnerName}
                             </span>
                         </h6> 
                         <h6>
                             Current Owner's Location: 
-                            <span>
+                            <span className="currentLocation">
                                 {this.state.OwnerLocation}
                             </span>
                         </h6> 
