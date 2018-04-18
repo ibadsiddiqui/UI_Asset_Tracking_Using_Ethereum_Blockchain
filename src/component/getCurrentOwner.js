db0e1fe0965a7bd9e232ec934d095d77669fc5bc
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-04-19 00:19:14 +0500

    comments added to component of current owners

--- src/component/getCurrentOwner.js
+++ src/component/getCurrentOwner.js
@@ -1,22 +1,33 @@
+// import react component classes
 import React, { Component } from 'react';
 import './css/getCurrentOwner.css';
 
+// contract things
 import Automobile_Contract from '../auto_mobile';
 import web3 from '../web3';
 
 export default class GetCurrentOwner extends Component {
-    state = {}
     constructor(){
         super();
+
+        // defines the state for class
+        this.state = {}
+
+        // binds the function to the submit button        
         this.getCurrentOwner = this.getCurrentOwner.bind(this);
     }
 
     async getCurrentOwner(e) {
         e.preventDefault();
+
+        // deconstructs the state object for VIN of asset
         const {VINNumber} = this.state;
 
+        // gets the current owner of the asset
         await Automobile_Contract.methods.getCurrentOwnerOfAsset(parseInt(VINNumber)).call()
             .then(result => {
+                
+                // updates the state
                 this.setState({
                     OwnerName: result[0],
                     OwnerLocation: result[1]
