63d8223141e1d6b8c055fc98eae594eb17b205e6
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-04-19 00:19:29 +0500

    comments added to prvious owner component

--- src/component/getPreviousOwner.js
+++ src/component/getPreviousOwner.js
@@ -1,29 +1,44 @@
+// import react component classes
 import React, { Component } from 'react';
 
+// contract things
 import Automobile_Contract from '../auto_mobile';
 import web3 from '../web3';
 
 export default class PreviousOwners extends Component {
     constructor(){
         super();
+
+        // defines the state for class
         this.state = {
             assetVIN:'',
             PreviousOwnersInfo:{},
         };
 
+        // binds the function to the submit button
         this.getPreviousOwner = this.getPreviousOwner.bind(this)
     }
-    async getPreviousOwner() {
+
+    async getPreviousOwner(e) {
+        e.preventDefault();
+
+        // deconstructs the state object for VIN of asset
         const {assetVIN} = this.state
+
+        // gets the list of previous owners
         await Automobile_Contract.methods.getPreviousOwnerOfAsset(parseInt(assetVIN))
             .call()
             .then( result => 
+
+                // updates the state
                 this.setState({
                     PreviousOwnersInfo: result
                 })
-                );
+
+            );
     }
     render(){
+        // deconstructs the state object for previous onwer of asset        
         const {PreviousOwnersInfo} = this.state;
 
         return(
