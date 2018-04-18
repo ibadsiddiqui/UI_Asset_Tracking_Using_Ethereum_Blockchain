9e0096da8c5e75c2d5cebf12df87b12abb1158c6
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-04-19 00:18:50 +0500

    comments added to getAssetDetails component

--- src/component/getAssetComponent.js
+++ src/component/getAssetComponent.js
@@ -1,3 +1,4 @@
+// import react component classes
 import React, { Component } from 'react';
 import '../App.css';
 
@@ -8,41 +9,60 @@ import PreviousOwners from './getPreviousOwner';
 import Automobile_Contract from '../auto_mobile';
 import web3 from '../web3';
 
+// for saving accounts from web3
 let accounts;
 
 export default class GetAsset extends Component {
     constructor() {
         super();
+
+        // defines the state for class
         this.state = {
             list_of_asset:[],
             manufacturer_name:"",
         };
+
+        // binds the function to the submit button
         this.getAsset = this.getAsset.bind(this);
     }
 
 
     async componentDidMount(){
+        // gets the details of manufacturer
         const manufacturer_details = await Automobile_Contract.methods.getManufacturerName().call();
+
+        // updates the state
         this.setState({
           manufacturer_name : manufacturer_details[0],
         });
+
+        // gets the account from the web3 service
         accounts = await web3.eth.getAccounts();
         console.log(accounts);
     }
 
     async getAsset(e){
         e.preventDefault();
+
+        // gets the list of manufactured assets by the manufacturer
         await Automobile_Contract.methods.getListOfAssetsOwnedByManufacturer().call()
-          .then( (result) =>{
+        .then( (result) =>{
+            
+            // updates the list
             this.setState({
               list_of_asset: [...result]
             });
-          })
+          
+        })
         console.log(this.state.list_of_asset);  
     }
     render(){
+        // deconstruct the list from the state
         const {list_of_asset} = this.state;
+
+        // maps the list to li-tags
         const list = list_of_asset.map((value) => <li className="list-group-item" key={value.toString()}>{value}</li>)
+
         return (
             <div className="col">
                 <div className="card difference">
